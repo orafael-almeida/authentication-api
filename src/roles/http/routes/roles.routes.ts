8bebb95067b927f248067f972839029cce92abcb
Rafael Almeida <almeidarp.dev@gmail.com>
2025-01-22 09:07:24 -0300

    feat: create findAll method in RolesRepository

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -3,11 +3,17 @@ import { Router } from 'express'
 
 const rolesRouter = Router()
 const rolesRepository = new RolesRepository()
+
 rolesRouter.post('/', (request, response) => {
   const { name } = request.body
   const role = rolesRepository.create({ name })
 
   return response.status(201).json(role)
+})
+
+rolesRouter.get('/', (request, response) => {
+  const roles = rolesRepository.findAll()
+  return response.json(roles)
 })
 
 export { rolesRouter }
